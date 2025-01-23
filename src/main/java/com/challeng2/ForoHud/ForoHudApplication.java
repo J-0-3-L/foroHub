07bcb3420ad5aa70bcfa5e8c8561450feac453e8
Joel Lawi <abecedegluu@gmail.com>
2025-01-23 16:42:49 -0500

    Add: Uso de dotenv

--- src/main/java/com/challeng2/ForoHud/ForoHudApplication.java
+++ src/main/java/com/challeng2/ForoHud/ForoHudApplication.java
@@ -3,10 +3,16 @@ package com.challeng2.ForoHud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ForoHudApplication {
-
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	
 		SpringApplication.run(ForoHudApplication.class, args);
 	}
 
